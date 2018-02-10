546bfb3e2cab0a580b7fa5b4f1883fe052924954
zwalla <tomstlaurent.mail@gmail.com>
2018-02-10 03:32:58 -0500

    Update oracle_daemon.js

--- oracle_daemon.js
+++ oracle_daemon.js
@@ -6,19 +6,35 @@ const provider = 'http://127.0.0.1:8545';//address of node
 
 const eth = new Eth(new Eth.HttpProvider(provider));
 
-//provider account
-const privateKeyString = "aa4b1ff1a70c4c55b9c5bd7bacb5fdd24dcc3e659f877faef7df3a34b0b5af98";//add ethereum private key(must have test ether. can export from metamask or myetherwallet. for public testnet, rinkeby faucet at https://www.rinkeby.io/#faucet)
-const account = privateToAccount(privateKeyString);//create account object from private key string
+/*
+provider account
+*/
+//add ethereum private key(must have test ether. can export from metamask or myetherwallet. for public testnet, rinkeby faucet at https://www.rinkeby.io/#faucet)
+const privateKeyString = "aa4b1ff1a70c4c55b9c5bd7bacb5fdd24dcc3e659f877faef7df3a34b0b5af98";
 
-//contract
-const eventEmitterFile = fs.readFileSync("event_emitter_abi.json");//load contract interface file
-const emitterAbi = JSON.parse(eventEmitterFile);//parse interface text to json object
-const emitterAddress = "0x856171b4c83ac0a9dd78b8a1062f3ca878db6928";//contract address(deployed to rinkeby testnet)
+//create account object from private key string
+const account = privateToAccount(privateKeyString);
+
+/*
+contract
+*/
+
+//load contract interface file
+const eventEmitterFile = fs.readFileSync("event_emitter_abi.json");
+
+//parse interface text to json object
+const emitterAbi = JSON.parse(eventEmitterFile);
+
+//contract address(deployed to rinkeby testnet)
+const emitterAddress = "0x856171b4c83ac0a9dd78b8a1062f3ca878db6928";
+
+//instantiate contract object
 const eventEmitterContract = eth.contract(emitterAbi).at(emitterAddress);
 
 //make contract fire desired query with oracleAddress
 function triggerContractQuery(queryString, oracleAddress){
 
+    //call contract fireEvent method
     EventEmitter.fireEvent(queryString, oracleAddress).then((success) =>{
         console.log(null, success);
 
@@ -31,6 +47,7 @@ function triggerContractQuery(queryString, oracleAddress){
 //respond to contract query
 function respondContractQuery(responseString){
 
+    //call contract callback method
     EventEmitter.callback(responseString).then((success) =>{
         console.log(null, success);
 
